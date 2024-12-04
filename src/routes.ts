5d815dbb36cef458b02d9143a71b850841c45889
PedroMakengo <pedromakengomiguel@gmail.com>
2024-12-04 09:13:03 +0000

    Deletando usuário

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { RemoveUserController } from './controllers/user/RemoveUserController'
 
 const router = Router()
 
@@ -10,5 +11,6 @@ const router = Router()
 router.post('/user', new CreateUserController().handle)
 router.post('/session', new AuthController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.delete('/user/remove', new RemoveUserController().handle)
 
 export { router }
