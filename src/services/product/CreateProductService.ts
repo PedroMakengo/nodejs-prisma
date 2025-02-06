6d5b0dcdb646cfc5968cc52a632403733e4bb205
PedroMakengo <pedromakengomiguel@gmail.com>
2025-02-06 09:15:00 +0000

    Criado o arquivo de service para registro de produto

---
+++ src/services/product/CreateProductService.ts
@@ -0,0 +1,8 @@
+import prismaClient from '../../prisma'
+import { ProductRequest } from '../../models/interfaces/product/ProductRequest'
+
+class CreateProductService {
+  async execute() {}
+}
+
+export { CreateProductService }
